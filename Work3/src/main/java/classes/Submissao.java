1dc873826662311a071922b0d97f34a1fc41e6a8
Brandow <brandowbuenosaires@gmail.com>
2019-11-06 20:26:47 -0300

    feat: add Submissao.java

--- Work3/src/main/java/classes/Submissao.java
+++ Work3/src/main/java/classes/Submissao.java
@@ -4,6 +4,76 @@ package classes;
  * Submissao
  */
 public abstract class Submissao {
+    protected String tituloSubmissao;
+    protected Situacao situacaoSubmissao;
+    protected String autoresSubmissao;
+    protected int MAX_AUTORES;
 
-    
+    public Submissao(String tituloSubmissao, Situacao situacaoSubmissao, String autoresSubmissao, int MAX_AUTORES) {
+        this.tituloSubmissao = tituloSubmissao;
+        this.situacaoSubmissao = situacaoSubmissao;
+        this.autoresSubmissao = autoresSubmissao;
+        this.MAX_AUTORES = MAX_AUTORES;
+    }
+
+    /**
+     * @return the tituloSubmissao
+     */
+    public String getTituloSubmissao() {
+        return tituloSubmissao;
+    }
+
+    /**
+     * @param tituloSubmissao the tituloSubmissao to set
+     */
+    public void setTituloSubmissao(String tituloSubmissao) {
+        this.tituloSubmissao = tituloSubmissao;
+    }
+
+    /**
+     * @return the situacaoSubmissao
+     */
+    public Situacao getSituacaoSubmissao() {
+        return situacaoSubmissao;
+    }
+
+    /**
+     * @param situacaoSubmissao the situacaoSubmissao to set
+     */
+    public void setSituacaoSubmissao(Situacao situacaoSubmissao) {
+        this.situacaoSubmissao = situacaoSubmissao;
+    }
+
+    /**
+     * @return the autoresSubmissao
+     */
+    public String getAutoresSubmissao() {
+        return autoresSubmissao;
+    }
+
+    /**
+     * @param autoresSubmissao the autoresSubmissao to set
+     */
+    public void setAutoresSubmissao(String autoresSubmissao) {
+        this.autoresSubmissao = autoresSubmissao;
+    }
+
+    /**
+     * @return the mAX_AUTORES
+     */
+    public int getMAX_AUTORES() {
+        return MAX_AUTORES;
+    }
+
+    /**
+     * @param mAX_AUTORES the mAX_AUTORES to set
+     */
+    public void setMAX_AUTORES(int mAX_AUTORES) {
+        MAX_AUTORES = mAX_AUTORES;
+    }
+
+    public toString() {
+        return this.tituloSubmissao +" "+ this.situacaoSubmissao +" "+ this.autoresSubmissao + " " + this.MAX_AUTORES;
+    }
+
 }
