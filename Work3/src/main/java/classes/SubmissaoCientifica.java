586aa04ecd61dbe1172c15ae9f00bf9c6a451676
Brandow <brandowbuenosaires@gmail.com>
2019-11-06 21:02:00 -0300

    feat: change constructor for super use

--- Work3/src/main/java/classes/SubmissaoCientifica.java
+++ Work3/src/main/java/classes/SubmissaoCientifica.java
@@ -3,11 +3,13 @@ package classes;
 /**
  * SubmissaoCientifica
  */
-public abstract class SubmissaoCientifica {
+public abstract class SubmissaoCientifica extends Submissao {
     protected String instituicao[];
     protected String palavraChave[];
 
-    public SubmissaoCientifica(String instituicao[], String palavraChave[]) {
+    public SubmissaoCientifica(String tituloSubmissao, Situacao situacaoSubmissao, String autoresSubmissao[],
+            int MAX_AUTORES, String instituicao[], String palavraChave[]) {
+        super(tituloSubmissao, situacaoSubmissao, autoresSubmissao, MAX_AUTORES);
         this.instituicao = instituicao;
         this.palavraChave = palavraChave;
     }
@@ -40,7 +42,7 @@ public abstract class SubmissaoCientifica {
         this.palavraChave = palavraChave;
     }
 
-    public String toString () {
+    public String toString() {
         return this.instituicao + " " + this.palavraChave;
     }
 
