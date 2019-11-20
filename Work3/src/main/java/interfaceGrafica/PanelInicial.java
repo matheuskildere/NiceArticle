913f7f779e70c58d0e98e937c01dd6aa647d6071
Brandow <brandowbuenosaires@gmail.com>
2019-11-20 14:43:34 -0300

    feat: changes in PanelInicial.java (design)

--- Work3/src/main/java/interfaceGrafica/PanelInicial.java
+++ Work3/src/main/java/interfaceGrafica/PanelInicial.java
@@ -1,66 +1,155 @@
 package interfaceGrafica;
 
-import java.awt.Component;
-import java.awt.Font;
-
-import javax.swing.JComboBox;
-import javax.swing.JLabel;
+import java.awt.*;
+import javax.swing.*;
 
 /**
  * PanelInicial
  */
-public class PanelInicial extends PanelPrincipal{
+public class PanelInicial extends PanelPrincipal {
+
+
+    private JLabel labelAll;
+    private JComboBox cbFiltroTipo;
+    private JComboBox cbFiltroSituacao;
+    
+    private JLabel lTitulo;
+    private JLabel lAutor;
+    private JLabel lSituacao;
+
+    private JButton bEditar;
+    private JButton bExcluir;
 
     private JLabel init_Titulo;
     private JLabel init_Autor;
     private JLabel init_Categoria;
     private int lab = 0;
 
-    private JComboBox comboBox;
-    /**
-     *
-     */
+   
+ 
     private static final long serialVersionUID = 1L;
 
     public PanelInicial() {
         removeAll();
+
+
+        labelAll = new JLabel(" Filtros");
+        labelAll.setBounds(50, 10, 200, 60);
+        labelAll.setFont(new Font("Muli", Font.PLAIN, 20));
+        labelAll.setIcon(new javax.swing.ImageIcon(getClass().getResource("icons/funnel.png")));
+        labelAll.setForeground(cor.branco());
+        add(labelAll);   
+
+        cbFiltroTipo = new JComboBox<>();
+        cbFiltroTipo.setBounds(140, 10, 100, 60);
+        cbFiltroTipo.setFont(new Font("Muli", Font.PLAIN, 15));
+        cbFiltroTipo.addItem("Todos");
+        cbFiltroTipo.addItem("Artigo");
+        cbFiltroTipo.addItem("Resumo");
+        cbFiltroTipo.addItem("Palestra");
+        cbFiltroTipo.addItem("Mini Curso");
+        cbFiltroTipo.addItem("Monografia");
+        cbFiltroTipo.addItem("Relatório técnico");
+        add(cbFiltroTipo);
+
+        cbFiltroSituacao = new JComboBox<>();
+        cbFiltroSituacao.setBounds(250, 10, 100, 60);
+        cbFiltroSituacao.setFont(new Font("Muli", Font.PLAIN, 15));
+        cbFiltroSituacao.addItem("Todos");
+        cbFiltroSituacao.addItem("Aprovados");
+        cbFiltroSituacao.addItem("Sob Avaliação");
+        cbFiltroSituacao.addItem("Reprovados");
+        add(cbFiltroSituacao);
+
+        lTitulo = new JLabel(" Titulo");
+        lTitulo.setBounds(50, 100, 200, 60);
+        lTitulo.setFont(new Font("Muli", Font.PLAIN, 23));
+        lTitulo.setIcon(new javax.swing.ImageIcon(getClass().getResource("icons/folder.png")));
+        lTitulo.setForeground(cor.branco());
+        add(lTitulo);
+
+        lAutor = new JLabel(" Autor");
+        lAutor.setBounds(500, 100, 200, 60);
+        lAutor.setFont(new Font("Muli", Font.PLAIN, 23));
+        lAutor.setIcon(new javax.swing.ImageIcon(getClass().getResource("icons/user.png")));
+        lAutor.setForeground(cor.branco());
+        add(lAutor);
+
+        lSituacao = new JLabel(" Situação");
+        lSituacao.setBounds(950, 100, 200, 60);
+        lSituacao.setFont(new Font("Muli", Font.PLAIN, 23));
+        lSituacao.setIcon(new javax.swing.ImageIcon(getClass().getResource("icons/situation.png")));
+        lSituacao.setForeground(cor.branco());
+        add(lSituacao);
+
+        
+        bEditar = new JButton("  |");
+        bEditar.setBounds(1215,180,80,40);
+        bEditar.setFont(new Font("Muli", Font.PLAIN, 26));
+        bEditar.setForeground(cor.branco());
+        bEditar.setIcon(new javax.swing.ImageIcon(getClass().getResource("icons/edit.png")));
+        bEditar.setBorderPainted(false);
+        bEditar.setFocusPainted(false);
+        add(bEditar);
+        
+        bExcluir = new JButton("");
+        bExcluir.setBounds(1290,180,40,40);
+        bExcluir.setIcon(new javax.swing.ImageIcon(getClass().getResource("icons/garbage.png")));
+        bExcluir.setBorderPainted(false);
+        bExcluir.setBorderPainted(false);
+        add(bExcluir);
+
+
+
+
         labelsAndFields();
     }
 
-    private void jComboBox(){
+    private void jComboBox() {
+     /*
         comboBox = new JComboBox<>();
-        comboBox.setBounds(50,450 , 100, 100);
+        comboBox.setBounds(50, 450, 100, 100);
         comboBox.setFont(new Font("Muli", Font.PLAIN, 19));
         comboBox.setBorder(null);
         add(comboBox);
+
+        */
     }
 
     @Override
     protected void labelsAndFields() {
-        settingsLabels(init_Titulo, "Título",1);
-        settingsLabels(init_Autor, "Autor",2);
-        settingsLabels(init_Categoria, "Categoria",3);
-        
-        settingsLabels(init_Titulo, "Inteligencia artificial",1);
-        settingsLabels(init_Autor, "matheus kidere",2);
-        settingsLabels(init_Categoria, "aprovado",3);
+
+        settingsLabels(init_Titulo, "Inteligência Artificial na sociedade", 1);
+        settingsLabels(init_Autor, "Matheus Kildere", 2);
+        settingsLabels(init_Categoria, "Aprovado", 3);
+
+        settingsLabels(init_Titulo, "Inteligência Artificial", 1);
+        settingsLabels(init_Autor, "Matheus Kildere", 2);
+        settingsLabels(init_Categoria, "Em análise", 3);
+
+        settingsLabels(init_Titulo, "Inteligência Artificial", 1);
+        settingsLabels(init_Autor, "Matheus Kildere", 2);
+        settingsLabels(init_Categoria, "Reprovado", 3);
+
+        settingsLabels(init_Titulo, "Inteligência Artificial", 1);
+        settingsLabels(init_Autor, "Matheus Kildere", 2);
+        settingsLabels(init_Categoria, "Aprovado", 3);
     }
 
     @Override
-    protected void settingsLabels(JLabel l_, String nome,int tipo) {
+    protected void settingsLabels(JLabel l_, String nome, int tipo) {
         l_ = new JLabel(nome);
-        l_.setFont(new Font("Muli", 4, 23));
+        l_.setFont(new Font("Muli", 4, 20));
         l_.setForeground(cor.branco());
         if (tipo == 1) {
-            l_.setBounds(76, lab + 75, 250, 30);
-        }else if (tipo == 2) {
-            l_.setBounds(548, lab +75 , 250, 30);
-        }else if (tipo == 3) {
-            l_.setBounds(900, 75 + lab , 250, 30);
-            lab += 40;
+            l_.setBounds(50, lab + 180, 400, 30);
+        } else if (tipo == 2) {
+            l_.setBounds(500, lab + 180, 400, 30);
+        } else if (tipo == 3) {
+            l_.setBounds(950, 180 + lab, 400, 30);
+            lab += 50;
         }
         add(l_);
     }
-    
-    
+
 }
