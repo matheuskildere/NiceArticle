3d294c4c2f5b9864de8fd81e79eef811ee912757
Brandow <brandowbuenosaires@gmail.com>
2019-11-28 17:15:02 -0300

    feat: enhancemensts in user message

--- Work3/src/main/java/interfaceGrafica/PanelPalestra.java
+++ Work3/src/main/java/interfaceGrafica/PanelPalestra.java
@@ -14,19 +14,19 @@ import categoria.Palestra;
 /**
  * PanelResumo
  */
-public class PanelPalestra extends PanelPrincipal{
+public class PanelPalestra extends PanelPrincipal {
 
     /**
      *
      */
     private static final long serialVersionUID = 1L;
-    protected String [] vetor_Autores= new String[1];
+    protected String[] vetor_Autores = new String[1];
     private JLabel l_Duracao;
     private JLabel l_Curriculo;
 
-    protected void buttonsAdd(){
+    protected void buttonsAdd() {
         b_addAutores = new JButton("add");
-        settingsButtons(b_addAutores,1);
+        settingsButtons(b_addAutores, 1);
         b_addAutores.addActionListener(actionPerformed(b_addAutores));
 
         b_submeter = new JButton("Submeter");
@@ -34,36 +34,35 @@ public class PanelPalestra extends PanelPrincipal{
         b_submeter.addActionListener(actionPerformed(b_submeter));
     }
 
-
     @Override
-    protected void labelsAndFields(){
-        settingsLabels(l_Titulo, "Título",1);
+    protected void labelsAndFields() {
+        settingsLabels(l_Titulo, "Título", 1);
         settingsTxt(j_Titulo, 1);
-        settingsLabels(l_Duracao, "Duração",1);
+        settingsLabels(l_Duracao, "Duração", 1);
         settingsTxt(j_Duracao, 1);
-        settingsLabels(l_Autores, "Autores",1);
+        settingsLabels(l_Autores, "Autores", 1);
         settingsTxt(j_Autores, 2);
-        settingsLabels(l_Curriculo, "Curriculo",1);
+        settingsLabels(l_Curriculo, "Curriculo", 1);
         settingsTxtArea(j_Curriculo, 2);
-        settingsLabels(l_Resumo, "Resumo",2);
+        settingsLabels(l_Resumo, "Resumo", 2);
         settingsTxtArea(j_Resumo);
-        settingsLabels(l_Abstract, "Abstract",2);
-        settingsTxtArea(j_Abstract,1);
-        
+        settingsLabels(l_Abstract, "Abstract", 2);
+        settingsTxtArea(j_Abstract, 1);
+
     }
 
-    protected void settingsTxtArea(JTextArea j_, int tipo){
+    protected void settingsTxtArea(JTextArea j_, int tipo) {
         if (tipo == 1) {
-            if(implementaJtextField > 320){
+            if (implementaJtextField > 320) {
                 implementaJtextField = 0;
             }
             j_.setBounds(750, 63 + implementaJtextField, 478, 200);
-            
+
             implementaJtextField += 160;
         }
         if (tipo == 2) {
             j_.setBounds(114, 63 + implementaJtextField, 478, 115);
-            
+
             implementaJtextField += 160;
         }
         j_.setFont(new Font("Muli", Font.PLAIN, 19));
@@ -73,15 +72,15 @@ public class PanelPalestra extends PanelPrincipal{
         add(j_);
     }
 
-    protected void settingsButtons(JButton b_add, int tipo){
+    protected void settingsButtons(JButton b_add, int tipo) {
         if (tipo == 1) {
             if (implementaJtextField >= 160) {
                 implementaJtextField = 0;
             }
-            b_add.setBounds(532 ,223 +implementaJtextField, 60, 35);
+            b_add.setBounds(532, 223 + implementaJtextField, 60, 35);
             b_add.setFont(new Font("Muli", 4, 10));
-        } else if (tipo == 2){
-            b_add.setBounds(1120 ,373 +implementaJtextField, 110, 35);
+        } else if (tipo == 2) {
+            b_add.setBounds(1120, 373 + implementaJtextField, 110, 35);
             b_add.setFont(new Font("Muli", 4, 15));
         }
         b_add.setFocusPainted(false);
@@ -90,12 +89,11 @@ public class PanelPalestra extends PanelPrincipal{
         add(b_add);
         implementaJtextField += 80;
     }
-    
 
     @Override
-    protected ActionListener actionPerformed(JButton b){
+    protected ActionListener actionPerformed(JButton b) {
         if (b == b_addAutores) {
-            return new ActionListener() { 
+            return new ActionListener() {
                 public void actionPerformed(ActionEvent event) {
                     boolean conseguiu = false;
                     boolean avisou = false;
@@ -103,42 +101,52 @@ public class PanelPalestra extends PanelPrincipal{
                         if (vetor_Autores[i] == null && !conseguiu) {
                             vetor_Autores[i] = j_Autores.getText();
                             conseguiu = true;
-                        } else if(vetor_Autores[vetor_Autores.length -1] != null && !avisou){
+
+                            JOptionPane.showMessageDialog(null, "Autor " + j_Autores.getText() + " adicionado !");
+                            j_Autores.setText("");
+
+                        } else if (vetor_Autores[vetor_Autores.length - 1] != null && !avisou) {
                             JOptionPane.showMessageDialog(null, "Voce atingiu o numero maximo de autores (1)!");
                             avisou = true;
                         }
                     }
                 }
-            }; 
+            };
         }
         if (b == b_submeter) {
-            return new ActionListener() { 
+            return new ActionListener() {
                 public void actionPerformed(ActionEvent event) {
                     try {
-                        if (j_Titulo.getText().equals("")){
+                        if (j_Titulo.getText().equals("")) {
                             throw new IllegalArgumentException("Sua palestra necessita de um Titulo");
-                        }else if (j_Duracao.getText().equals("")){
+                        } else if (j_Duracao.getText().equals("")) {
                             throw new IllegalArgumentException("Sua Palestra necessita de uma duracao");
-                        }else if (vetor_Autores[0] == null) {
-                            throw new IllegalArgumentException("E necessário informar ao menos um autor"); 
-                        }else if (j_Curriculo.getText().equals("")){
+                        } else if (vetor_Autores[0] == null) {
+                            throw new IllegalArgumentException("E necessário informar ao menos um autor");
+                        } else if (j_Curriculo.getText().equals("")) {
                             throw new IllegalArgumentException("Voce precisa informar seu curriculo");
-                        }else if (j_Resumo.getText().equals("")){
+                        } else if (j_Resumo.getText().equals("")) {
                             throw new IllegalArgumentException("Sua Palestra necessita de um resumo");
-                        }else if (j_Abstract.getText().equals("")){
+                        } else if (j_Abstract.getText().equals("")) {
                             throw new IllegalArgumentException("Your talk needs a summary");
-                        }else {
-                            Situacao[] options = {Situacao.APROVADO,Situacao.SOB_AVALIACAO,Situacao.REPROVADO};
-                            int x = JOptionPane.showOptionDialog(null, "Situacao da Submissao", "Informe", JOptionPane.DEFAULT_OPTION,JOptionPane.QUESTION_MESSAGE,null, options, null);
+                        } else {
+                            Situacao[] options = { Situacao.APROVADO, Situacao.SOB_AVALIACAO, Situacao.REPROVADO };
+                            int x = JOptionPane.showOptionDialog(null, "Situacao da Submissao", "Informe",
+                                    JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, null);
                             int duracao = Integer.parseInt(j_Duracao.getText());
-                            submissao = new Palestra(j_Titulo.getText(), options[x], vetor_Autores, j_Resumo.getText(),j_Abstract.getText(), duracao, j_Curriculo.getText());
+                            submissao = new Palestra(j_Titulo.getText(), options[x], vetor_Autores, j_Resumo.getText(),
+                                    j_Abstract.getText(), duracao, j_Curriculo.getText());
                             listaSubmissao.incluir(submissao);
-                            clearAllText();
+                            JOptionPane.showMessageDialog(null,
+                                    "A palestra " + j_Titulo.getText() + " foi adicionada com sucesso !");
                         }
+
+                        clearAllText();
+
                     } catch (IllegalArgumentException e) {
                         JOptionPane.showMessageDialog(null, e.getMessage());
                     }
-                } 
+                }
             };
         }
         return null;
--- Work3/src/main/java/interfaceGrafica/PanelResumo.java
+++ Work3/src/main/java/interfaceGrafica/PanelResumo.java
@@ -15,37 +15,37 @@ import categoria.Resumo;
 /**
  * PanelResumo
  */
-public class PanelResumo extends PanelPrincipal{
+public class PanelResumo extends PanelPrincipal {
 
     /**
      *
      */
     private static final long serialVersionUID = 1L;
-    protected String [] vetor_Autores= new String[8];
-    protected String [] vetor_Instituicoes= new String[8];
-    protected String [] vetor_PalavrasC= new String[4];
+    protected String[] vetor_Autores = new String[8];
+    protected String[] vetor_Instituicoes = new String[8];
+    protected String[] vetor_PalavrasC = new String[4];
 
     @Override
-    protected void labelsAndFields(){
-        settingsLabels(l_Titulo, "Título",1);
+    protected void labelsAndFields() {
+        settingsLabels(l_Titulo, "Título", 1);
         settingsTxt(j_Titulo, 1);
-        settingsLabels(l_Autores, "Autores",1);
+        settingsLabels(l_Autores, "Autores", 1);
         settingsTxt(j_Autores, 2);
-        settingsLabels(l_Instituicoeseld, "Instituições",1);
+        settingsLabels(l_Instituicoeseld, "Instituições", 1);
         settingsTxt(j_Instituicoes, 2);
-        settingsLabels(l_PalavrasC, "Palavras Chaves",1);
+        settingsLabels(l_PalavrasC, "Palavras Chaves", 1);
         settingsTxt(j_PalavrasC, 2);
     }
 
-    protected void settingsButtons(JButton b_add, int tipo){
+    protected void settingsButtons(JButton b_add, int tipo) {
         if (tipo == 1) {
             if (implementaJtextField > 240) {
                 implementaJtextField = 0;
             }
-            b_add.setBounds(870 ,153 +implementaJtextField, 70, 35);
+            b_add.setBounds(870, 153 + implementaJtextField, 70, 35);
             b_add.setFont(new Font("Muli", 4, 10));
-        } else if (tipo == 2){
-            b_add.setBounds(650 ,153 +implementaJtextField, 110, 35);
+        } else if (tipo == 2) {
+            b_add.setBounds(650, 153 + implementaJtextField, 110, 35);
             b_add.setFont(new Font("Muli", 4, 15));
         }
         b_add.setFocusPainted(false);
@@ -56,21 +56,21 @@ public class PanelResumo extends PanelPrincipal{
     }
 
     @Override
-    protected void settingsTxt(JTextField j_, int tipo){
+    protected void settingsTxt(JTextField j_, int tipo) {
         if (tipo == 1) {
             j_.setBounds(430, 63 + implementaJtextField, 510, 35);
-        }else if (tipo == 2) {
+        } else if (tipo == 2) {
             j_.setBounds(430, 63 + implementaJtextField, 434, 35);
         }
-        
+
         j_.setFont(new Font("Muli", Font.PLAIN, 19));
         j_.setBorder(null);
         add(j_);
         implementaJtextField += 90;
     }
-    
+
     @Override
-    protected void settingsLabels(JLabel l_, String nome, int tipo){ 
+    protected void settingsLabels(JLabel l_, String nome, int tipo) {
         l_ = new JLabel(nome);
         if (tipo == 1) {
             l_.setBounds(430, 28 + implementaJlabels, 250, 30);
@@ -82,9 +82,9 @@ public class PanelResumo extends PanelPrincipal{
     }
 
     @Override
-    protected ActionListener actionPerformed(JButton b){
+    protected ActionListener actionPerformed(JButton b) {
         if (b == b_addAutores) {
-            return new ActionListener() { 
+            return new ActionListener() {
                 public void actionPerformed(ActionEvent event) {
                     boolean conseguiu = false;
                     boolean avisou = false;
@@ -92,16 +92,19 @@ public class PanelResumo extends PanelPrincipal{
                         if (vetor_Autores[i] == null && !conseguiu) {
                             vetor_Autores[i] = j_Autores.getText();
                             conseguiu = true;
-                        } else if(vetor_Autores[vetor_Autores.length -1] != null && !avisou){
+                            JOptionPane.showMessageDialog(null, "Autor " + j_Autores.getText() + " adicionado !");
+                            j_Autores.setText("");
+
+                        } else if (vetor_Autores[vetor_Autores.length - 1] != null && !avisou) {
                             JOptionPane.showMessageDialog(null, "Voce atingiu o numero maximo de autores (8)!");
                             avisou = true;
                         }
                     }
                 }
-            }; 
+            };
         }
         if (b == b_addInstituicoes) {
-            return new ActionListener() { 
+            return new ActionListener() {
                 public void actionPerformed(ActionEvent event) {
                     boolean conseguiu = false;
                     boolean avisou = false;
@@ -109,16 +112,21 @@ public class PanelResumo extends PanelPrincipal{
                         if (vetor_Instituicoes[i] == null && !conseguiu) {
                             vetor_Instituicoes[i] = j_Instituicoes.getText();
                             conseguiu = true;
-                        } else if(vetor_Instituicoes[vetor_Instituicoes.length -1] != null && !avisou){
+
+                            JOptionPane.showMessageDialog(null,
+                                    "Instituição " + j_Instituicoes.getText() + " adicionada !");
+                            j_Instituicoes.setText("");
+
+                        } else if (vetor_Instituicoes[vetor_Instituicoes.length - 1] != null && !avisou) {
                             JOptionPane.showMessageDialog(null, "Voce atingiu o numero maximo de instituicoes (8)!");
                             avisou = true;
                         }
                     }
-                } 
+                }
             };
         }
         if (b == b_addPalavrasC) {
-            return new ActionListener() { 
+            return new ActionListener() {
                 public void actionPerformed(ActionEvent event) {
                     boolean conseguiu = false;
                     boolean avisou = false;
@@ -126,37 +134,50 @@ public class PanelResumo extends PanelPrincipal{
                         if (vetor_PalavrasC[i] == null && !conseguiu) {
                             vetor_PalavrasC[i] = j_PalavrasC.getText();
                             conseguiu = true;
-                        } else if(vetor_PalavrasC[vetor_PalavrasC.length -1] != null && !avisou){
+
+                            JOptionPane.showMessageDialog(null,
+                                    "Palavra chave " + j_PalavrasC.getText() + " adicionada !");
+                            j_PalavrasC.setText("");
+
+                        } else if (vetor_PalavrasC[vetor_PalavrasC.length - 1] != null && !avisou) {
                             JOptionPane.showMessageDialog(null, "Voce atingiu o numero maximo de Palavras Chaves (8)!");
                             avisou = true;
                         }
                     }
-                } 
+                }
             };
         }
         if (b == b_submeter) {
-            return new ActionListener() { 
+            return new ActionListener() {
                 public void actionPerformed(ActionEvent event) {
                     try {
-                        if (j_Titulo.getText().equals("")){
+                        if (j_Titulo.getText().equals("")) {
                             throw new IllegalArgumentException("Seu resumo necessita de um Titulo");
-                        }else if (vetor_Autores[0] == null) {
-                            throw new IllegalArgumentException("E necess�rio informar ao menos um autor"); 
-                        }else if(vetor_Instituicoes[0] == null){
-                            throw new IllegalArgumentException("E necess�rio informar uma instituicao ligada ao trabalho"); 
-                        }else if (vetor_PalavrasC[0] == null){
-                            throw new IllegalArgumentException("Coloque ao menos uma palavra chave relacionada ao seu resumo"); 
-                        }else {
-                            Situacao[] options = {Situacao.APROVADO,Situacao.SOB_AVALIACAO,Situacao.REPROVADO};
-                            int x = JOptionPane.showOptionDialog(null, "Situacao da Submissao", "Informe", JOptionPane.DEFAULT_OPTION,JOptionPane.QUESTION_MESSAGE,null, options, null);
-                            submissao = new Resumo(j_Titulo.getText(), options[x], vetor_Autores, vetor_Instituicoes, vetor_PalavrasC);
+                        } else if (vetor_Autores[0] == null) {
+                            throw new IllegalArgumentException("E necess�rio informar ao menos um autor");
+                        } else if (vetor_Instituicoes[0] == null) {
+                            throw new IllegalArgumentException(
+                                    "E necess�rio informar uma instituicao ligada ao trabalho");
+                        } else if (vetor_PalavrasC[0] == null) {
+                            throw new IllegalArgumentException(
+                                    "Coloque ao menos uma palavra chave relacionada ao seu resumo");
+                        } else {
+                            Situacao[] options = { Situacao.APROVADO, Situacao.SOB_AVALIACAO, Situacao.REPROVADO };
+                            int x = JOptionPane.showOptionDialog(null, "Situacao da Submissao", "Informe",
+                                    JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, null);
+                            submissao = new Resumo(j_Titulo.getText(), options[x], vetor_Autores, vetor_Instituicoes,
+                                    vetor_PalavrasC);
                             listaSubmissao.incluir(submissao);
+                            JOptionPane.showMessageDialog(null,
+                                    "O resumo " + j_Titulo.getText() + " foi adicionado com sucesso !");
+
                             clearAllText();
+
                         }
                     } catch (IllegalArgumentException e) {
                         JOptionPane.showMessageDialog(null, e.getMessage());
                     }
-                } 
+                }
             };
         }
         return null;
